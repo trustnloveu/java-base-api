d24f7d0ab3b86c4d21649ba4df83af144790e324
ejyang <trustnloveu@gmail.com>
2023-05-23 15:56:56 +0900

    gradle 재설정

--- main-api/src/main/java/kr/co/ejyang/MainApplication.java
+++ main-api/src/main/java/kr/co/ejyang/MainApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class MainApplication {
 
     public static void main(String[] args) {
-        // 프로퍼티 설정 ( main-api, module-file )
-//        System.setProperty("spring.config.name", "application, application-file");
-
         // run
         SpringApplication.run(MainApplication.class, args);
     }
